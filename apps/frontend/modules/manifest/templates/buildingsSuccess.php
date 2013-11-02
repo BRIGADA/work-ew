f2164fa8288b2454a5f7ce054cd1fbdc54efda5b
BRIGADA <BRIGADA@live.ru>
2013-11-02 15:20:00 +0400

    2013-11-01

--- apps/frontend/modules/manifest/templates/buildingsSuccess.php
+++ apps/frontend/modules/manifest/templates/buildingsSuccess.php
@@ -2,8 +2,13 @@
     <h1>Постройки</h1>
 </div>
 
-<ul>
+<ul class="thumbnails">
     <?php foreach ($buildings as $building) : ?>
-    <li><a href="<?php echo url_for("@manifest-building?type={$building->type}") ?>"><?php echo $building->type ?></a></li>
+    <li class="span3">
+        <div class="thumbnail">
+            <img style="max-height: 150px" src="<?php echo image_path('http://kabam1-a.akamaihd.net/edgeworld/images/buildings/'.  strtolower($building->type) .'_3.png') ?>"/>
+            <a href="<?php echo url_for("@manifest-building?type={$building->type}") ?>"><?php echo $building->type ?></a>
+        </div>
+    </li>
     <?php endforeach ?>
 </ul>
