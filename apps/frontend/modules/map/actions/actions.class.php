dfb113a04fda8f05e94d6f21322488306625e160
BRIGADA <BRIGADA@live.ru>
2013-11-11 12:26:22 +0400

    2013-11-11

--- apps/frontend/modules/map/actions/actions.class.php
+++ apps/frontend/modules/map/actions/actions.class.php
@@ -17,16 +17,37 @@ class mapActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-  	if(!$this->getUser()->hasAttribute('maps', 'api'))
+//  	if(!$this->getUser()->hasAttribute('maps', 'api'))
   	{
 	  	$r = $this->getUser()->RGET('/api/maps');
 	  	$this->redirectUnless($r, 'common/index');  	
 	  	$data = json_decode($r);
 	  	$this->redirectUnless($data, 'common/index');
 	  	$this->getUser()->setAttribute('maps', $data->response->maps, 'api');
+        
+        foreach($data->response->maps as $map) {
+
+          $record = Doctrine::getTable('Map')->find($map->id);
+          if(!$record) {
+            $record = new Map();
+            $record->id = $map->id;
+          }
+          $record->width = $map->width;
+          $record->height = $map->height;
+          $record->sector = $map->sector;
+          $record->chunk_size = $map->chunk_size;
+          $record->active = $map->active;
+          $record->type = $map->type;
+          $record->maximum_node_level = $map->maximum_node_level;
+          $record->max_territory_limit = $map->max_territory_limit;
+          $record->upgrade_costs = $map->upgrade_costs;
+          $record->outpost_levels = $map->outpost_levels;
+          
+          $record->save();
+        }
 	  	
   	}
-	 	$this->result = $this->getUser()->getAttribute('maps', null, 'api');
+	$this->result = $this->getUser()->getAttribute('maps', null, 'api');
   }
 
   public function executeShow(sfWebRequest $request)
